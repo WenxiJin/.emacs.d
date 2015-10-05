539b7600cb425a3b743b699bc90b1bc885036bf2
Wenxi Jin <wenxi.jin@microsemi.com>
2015-10-05 16:00:51 +0200

    Fix 4 space indentation

--- lisp/init-google-c-style.el
+++ lisp/init-google-c-style.el
@@ -3,7 +3,10 @@
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 ;; NOTE: overwrite c-basic-offset from 2 to 4
-(setq c-basic-offset 4)
+(defun my-c-mode-hook ()
+  (setq c-basic-offset 4))
+(add-hook 'c-mode-hook 'my-c-mode-hook)
+(add-hook 'c++-mode-hook 'my-c-mode-hook)
 
 
 (provide 'init-google-c-style)
