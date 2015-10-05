7e21f25bb6a76b87a46226fbc7f17deea234a270
Wenxi Jin <wenxi.jin@microsemi.com>
2015-10-05 14:27:01 +0200

    Added key binding for highlight-symbol-next/prev

--- lisp/init-highlight-symbol.el
+++ lisp/init-highlight-symbol.el
@@ -3,5 +3,9 @@
 (require 'highlight-symbol)
 (add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
 (setq highlight-symbol-on-navigation-p t)
+(global-set-key [(control f9)] 'highlight-symbol)
+(global-set-key [f9] 'highlight-symbol-next)
+(global-set-key [f10] 'highlight-symbol-prev)
+(global-set-key [(meta f9)] 'highlight-symbol-query-replace)
 
 (provide 'init-highlight-symbol)
