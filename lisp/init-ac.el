a0c1b176412adeefe73ae0e01746e9c4324e05cb
Wenxi Jin <wenxi.jin@microsemi.com>
2015-10-13 09:29:55 +0200

    Added ac-c-header

--- lisp/init-ac.el
+++ lisp/init-ac.el
@@ -1,7 +1,14 @@
 (require-package 'auto-complete)
+(require-package 'ac-c-headers)
 
 (require 'auto-complete-config)
 (ac-config-default)
+
+(add-hook 'c-mode-hook
+	  (lambda ()
+	    (add-to-list 'ac-sources 'ac-source-c-headers)
+	    (add-to-list 'ac-sources 'ac-source-c-header-symbols t)))
+
 
 
 (provide 'init-ac)
