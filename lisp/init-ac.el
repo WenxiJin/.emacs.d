53abf4754748a2c7e89b7ce319c701853c34538d
Wenxi Jin <jwenxi@gmail.com>
2015-10-13 22:41:44 +0200

    Added auto-complete-c-headers into c++-mode-hook

--- lisp/init-ac.el
+++ lisp/init-ac.el
@@ -4,11 +4,12 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
-(add-hook 'c-mode-hook
-	  (lambda ()
-	    (add-to-list 'ac-sources 'ac-source-c-headers)
-	    (add-to-list 'ac-sources 'ac-source-c-header-symbols t)))
+(defun my:ac-c-headers-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-sources-c-headers))
 
+(add-hook 'c++-mode-hook 'my:ac-c-headers-init)
+(add-hook 'c-mode-hook 'my:ac-c-headers-init)
 
 
 (provide 'init-ac)
