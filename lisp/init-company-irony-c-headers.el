8c860bd6e5d9f4b597cef364463744697afb9397
Wenxi Jin <wenxi.jin@microsemi.com>
2015-10-14 16:13:53 +0200

    Do not duplicate company-irony in company-backends

--- lisp/init-company-irony-c-headers.el
+++ lisp/init-company-irony-c-headers.el
@@ -2,9 +2,10 @@
 
 (require 'company-irony-c-headers)
 ;; Load with `irony-mode` as a grouped backend
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends '(company-irony-c-headers company-irony)))
+;; (eval-after-load 'company
+;;   '(add-to-list
+;;     'company-backends '(company-irony-c-headers company-irony)))
 
+(add-to-list 'company-backends 'company-irony-c-headers)
 
 (provide 'init-company-irony-c-headers)
