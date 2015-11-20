f22a172389a0b98ff4cf7ea290cf6586e3a0598f
Wenxi Jin <wenxi.jin@microsemi.com>
2015-11-20 14:08:46 +0100

    Added one more into .gitignore

--- custom.el
+++
@@ -1,9 +0,0 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-gtags-auto-update t)
- '(helm-gtags-ignore-case t)
- '(helm-gtags-path-style (quote relative)))
-
