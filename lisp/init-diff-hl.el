787853d8bce75dc6e8e070bc3004c57a098bcca8
Wenxi Jin <wenxi.jin@microsemi.com>
2015-11-30 17:47:01 +0100

    Config diff-hl according to spacemacs

--- lisp/init-diff-hl.el
+++ lisp/init-diff-hl.el
@@ -2,9 +2,11 @@
 
 
 (require 'diff-hl)
-(add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-(add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
-(diff-hl-flydiff-mode 1)
+(setq diff-hl-side 'right)
+(global-diff-hl-mode)
+(unless (display-graphic-p)
+  (setq diff-hl-side 'left)
+  (diff-hl-margin-mode))
 
 
 (provide 'init-diff-hl)
