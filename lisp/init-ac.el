08e6fca38dc40f38d8a9abc0a65bb607cf1a1ae9
Wenxi Jin <wenxi.jin@microsemi.com>
2015-11-30 18:34:19 +0100

    minor change in the ac

--- lisp/init-ac.el
+++ lisp/init-ac.el
@@ -5,15 +5,18 @@
 (require 'auto-complete-config)
 ;; use default config
 (ac-config-default)
-(setq ac-auto-start 4)
+(setq ac-auto-start 0
+      ac-delay 0.2
+      ac-quick-help-delay 1.
+      ac-use-fuzzy t
+      ac-fuzzy-enable t)
 
 ;;; set the trigger key so that it can work together with yasnippet on tab key,
 ;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
 ;;; activate, otherwise, auto-complete will
 (ac-set-trigger-key "TAB")
 (ac-set-trigger-key "<tab>")
-(setq ac-use-quick-help nil)
-(setq ac-menu-height 20)
+
 
 (defun my:ac-c-headers-init ()
   (require 'ac-c-headers)
