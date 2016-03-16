74f511e170110d26c61943f8cf8cbc304ae6891e
Wenxi Jin <wenxi.jin@microsemi.com>
2016-03-16 15:23:31 +0100

    Changed keybinding for highlight-symbol

--- lisp/init-airline.el
+++ lisp/init-airline.el
@@ -1,9 +1,9 @@
 (require-package 'airline-themes)
 
 (require 'airline-themes)
-(load-theme 'airline-light t)
+;; (load-theme 'airline-light t)
 ;; (load-theme 'airline-badwolf t)
 ;; (load-theme 'airline-papercolor t)
-
+(load-theme 'airline-dark t)
 
 (provide 'init-airline)
--- lisp/init-highlight-symbol.el
+++ lisp/init-highlight-symbol.el
@@ -3,9 +3,9 @@
 (require 'highlight-symbol)
 (add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
 (setq highlight-symbol-on-navigation-p t)
-;; (global-set-key [(control f9)] 'highlight-symbol)
-(global-set-key [f5] 'highlight-symbol-next)
-(global-set-key [f6] 'highlight-symbol-prev)
+(global-set-key [f9] 'highlight-symbol)
+;; (global-set-key [f5] 'highlight-symbol-next)
+;; (global-set-key [f6] 'highlight-symbol-prev)
 ;; (global-set-key [(meta f9)] 'highlight-symbol-query-replace)
 
 (provide 'init-highlight-symbol)
