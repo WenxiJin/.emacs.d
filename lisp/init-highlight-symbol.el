b37e6638596c3b72dd3b739e5a3aeecf3e98842f
Wenxi Jin <wenxi.jin@microsemi.com>
2016-03-21 15:06:59 +0100

    Re-enable f5 & f6 as highlight-symbol keybindings

--- lisp/init-highlight-symbol.el
+++ lisp/init-highlight-symbol.el
@@ -4,8 +4,8 @@
 (add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
 (setq highlight-symbol-on-navigation-p t)
 (global-set-key [f9] 'highlight-symbol)
-;; (global-set-key [f5] 'highlight-symbol-next)
-;; (global-set-key [f6] 'highlight-symbol-prev)
-;; (global-set-key [(meta f9)] 'highlight-symbol-query-replace)
+(global-set-key [f5] 'highlight-symbol-next)
+(global-set-key [f6] 'highlight-symbol-prev)
+(global-set-key [(meta f9)] 'highlight-symbol-query-replace)
 
 (provide 'init-highlight-symbol)
