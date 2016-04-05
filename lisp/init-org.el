3ebe347a3607cba8c2ff7d9c17ee1370b44abc8d
Wenxi Jin <wenxi.jin@microsemi.com>
2016-04-05 10:05:38 +0200

    Added markdown-mode, markdown-mode+, simplified f7 make cmd

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,5 +1,8 @@
 (require 'org)
 
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
 (setq org-log-done 'time)
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
