58074140b2bdef8f814ab573fcb8c1337f650f3d
Wenxi Jin <wenxi.jin@microsemi.com>
2016-05-31 15:32:40 +0200

    Changed default keybinding for er/expand-region, enabled pending-delete-mode by default

--- lisp/init-expand-region.el
+++ lisp/init-expand-region.el
@@ -1,8 +1,10 @@
 (require-package 'expand-region)
 
 (require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "C-c +") 'er/expand-region)
 ;; er/contract-region if you expand too far
 
+;; replace the marking region
+(pending-delete-mode t)
 
 (provide 'init-expand-region)
