21e31d66c959b957436d7f4e358568051d15f4ba
Wenxi Jin <wenxi.jin@microsemi.com>
2016-06-14 08:22:58 +0200

    Disable display-time-mode
    Hard to see the file name when it is enabled. And it is rarely used.

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -43,7 +43,6 @@
 (setq confirm-kill-emacs 'y-or-n-p)
 ;; set the default width of fill-column
 (setq-default fill-column 80)
-;; Display system time and load
-(display-time-mode t)
+
 
 (provide 'init-misc)
