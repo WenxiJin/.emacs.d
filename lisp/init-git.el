de9cf4cc97dc09c19ce2b83d7df38df99680fff9
Wenxi Jin <wenxi.jin@microsemi.com>
2016-06-15 11:04:48 +0200

    Fixed vc-annotate
    Applied patch for vc-annotate for git.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,20 +1,16 @@
-(require-package 'git)
-(require-package 'git-blame)
-
-(use-package git
-  :defer t
+(use-package vc-git
   :init
   (add-to-list 'load-path "/usr/share/git/core/emacs/git")
   :config
   (defun my-git-mode-hook ()
-    (require git-blame)
-    (require vc-git)
     (defun vc-git-annotate-command (file buf &optional rev)
       (let ((name (file-relative-name file)))
         (vc-git-command buf 'async nil "blame" "--date=iso" rev "--" name)))
+    (require 'git-blame)
+    (require 'vc-git)
     )
 
-  (my-git-mode-hook)
+  (add-hook 'prog-mode-hook 'my-git-mode-hook)
   )
 
 
