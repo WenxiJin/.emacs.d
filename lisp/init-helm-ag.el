3c69d03f760414ee92cec7ee2bd659d275517de1
Wenxi Jin <jwenxi@gmail.com>
2016-12-24 23:45:15 +0100

    Improved helm-ag performance
    Disabled helm-follow-mode-persistent
    Disabled helm-ag-insert-at-point

--- lisp/init-helm-ag.el
+++ lisp/init-helm-ag.el
@@ -1,12 +1,12 @@
 (require-package 'helm-ag)
 
-(custom-set-variables
- '(helm-follow-mode-persistent t))
+;; (custom-set-variables
+;;  '(helm-follow-mode-persistent t))
 
 (custom-set-variables
  '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
  '(helm-ag-command-option "--all-text")
- '(helm-ag-insert-at-point 'symbol)
+ ;; '(helm-ag-insert-at-point 'symbol)
  '(helm-ag-ignore-buffer-patterns '("\\.txt\\'" "\\.mkd\\'")))
 
 (require 'helm-ag)
