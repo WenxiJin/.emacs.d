b412a2134e1cb8999da652b75d188778238c5a9c
Wenxi Jin <jwenxi@gmail.com>
2016-12-24 23:35:40 +0100

    Added ignore buffer patterns in helm-ag

--- lisp/init-helm-ag.el
+++ lisp/init-helm-ag.el
@@ -6,7 +6,8 @@
 (custom-set-variables
  '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
  '(helm-ag-command-option "--all-text")
-  '(helm-ag-insert-at-point 'symbol))
+ '(helm-ag-insert-at-point 'symbol)
+ '(helm-ag-ignore-buffer-patterns '("\\.txt\\'" "\\.mkd\\'")))
 
 (require 'helm-ag)
 
