039e93912211672b1a611cfcd4303432126b52ac
Wenxi Jin <jwenxi@gmail.com>
2020-06-09 12:18:16 +0200

    Adjust access-label to align with MVS
    Adjust access-label to align with Microsoft Visual Studio

--- lisp/init-google-c-style.el
+++ lisp/init-google-c-style.el
@@ -4,7 +4,9 @@
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 ;; NOTE: overwrite c-basic-offset from 2 to 4
 (defun my-c-mode-hook ()
-  (setq c-basic-offset 4))
+  (setq c-basic-offset 4)
+  (c-set-offset 'access-label '-)
+  )
 (add-hook 'c-mode-hook 'my-c-mode-hook)
 (add-hook 'c++-mode-hook 'my-c-mode-hook)
 
